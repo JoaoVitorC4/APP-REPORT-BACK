288716af48d70a2984a19b9d15298bec6a355323
JoaoVitorC4 <jvcecato@hotmail.com>
2022-05-24 19:09:53 -0400

    formulario de reports

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('reports', ['App\Http\Controllers\ReportController', 'index']);
+
+Route::post('reports', ['App\Http\Controllers\ReportController', 'store']);
